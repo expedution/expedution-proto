b3a7653077d7f106f14568927cd32ddc006360f6
Romain <romain.cky@gmail.com>
2016-05-24 16:30:00 +0200

    added users show and edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:show]
+  resources :users
   resources :expeditions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
