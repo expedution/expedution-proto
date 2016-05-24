c5a6125c8df3eb0d0e2014ad4f7e61ffa11f8d0a
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-24 11:16:38 +0200

    Models + tests

--- app/models/expedition.rb
+++ app/models/expedition.rb
@@ -1,11 +1,12 @@
 class Expedition < ActiveRecord::Base
   belongs_to :user
   has_many :days
+  has_many :participations
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :starts_on, presence: true
-  validates :ends_on, presence: true
+  # validates :starts_on, presence: true
+  # validates :ends_on, presence: true
   # validates :capacity, presence: true
   # validates :theme, presence: true
 end
