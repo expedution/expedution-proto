e8be70a7feb373419d360597de6c425088f19337
Romain <romain.cky@gmail.com>
2016-05-24 12:36:29 +0200

    created users controller with index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
   root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
