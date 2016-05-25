540c5593f61d455747ca28c2df2c2b15efcb2db2
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-25 13:06:53 +0200

    added Prospect model & button to Request an invite

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+
   resources :users
   resources :expeditions
   # The priority is based upon order of creation: first created -> highest priority.
