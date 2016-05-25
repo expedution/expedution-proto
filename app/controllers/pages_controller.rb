db6cae425490345775b690a536b29f1ca9610fa8
Romain <romain.cky@gmail.com>
2016-05-25 11:26:33 +0200

    implementation de la structure de base du dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,26 @@
 class PagesController < ApplicationController
+  before_action :find_user, only: [:dashboard]
   skip_before_action :authenticate_user!, only: :home
   skip_after_action :verify_authorized, only: :home
+  skip_after_action :verify_authorized, only: :dashboard
 
 
   def home
   end
 
   def dashboard
+    find_user
+    @expeditions = current_user.expeditions.all
+  end
 
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :photo, :photo_cache)
   end
 end
