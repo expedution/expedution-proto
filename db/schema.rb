dd466a7276460acd4784cc7882e5a99c1803911e
Romain <romain.cky@gmail.com>
2016-05-26 11:25:26 +0200

    added Title in Days table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525110559) do
+ActiveRecord::Schema.define(version: 20160526092247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160525110559) do
     t.integer  "expedition_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "title"
   end
 
   add_index "days", ["expedition_id"], name: "index_days_on_expedition_id", using: :btree
