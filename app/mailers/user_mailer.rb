343f0b0cc5e72c69d1ddd46994411875baf2e3f7
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-26 17:08:06 +0200

    testing emailers on Heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,4 @@
 
-# Default #
-
-#   # Subject can be set in your I18n file at config/locales/en.yml
-#   # with the following lookup:
-#   #
-#   #   en.user_mailer.welcome.subject
-#   #
-#   def welcome
-#     @greeting = "Hi"
-
-#     mail to: "to@example.org"
-#   end
-# end
-
-
 
 class UserMailer < ApplicationMailer
   def welcome(user)
@@ -21,5 +6,10 @@ class UserMailer < ApplicationMailer
 
     mail(to: @user.email, subject: 'Welcome to your learning expedition')
     # This will render a view in `app/views/user_mailer`!
+  end
+
+  def expeditioncreation(user)
+    @user = user
+    mail(to: @user.email, subject: 'Your learning expedition is created')
   end
 end
