81aff70d335c68df8a574dba9f8a565e5ca2501e
Romain <romain.cky@gmail.com>
2016-05-30 14:54:10 +0200

    added create method and private method to feedback controller

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,7 +1,19 @@
 class FeedbackController < ApplicationController
-  def index
+  before_action :set_feedbacks, only: [:create]
+
+  def create
+    @feedback = feedback.new(set_feedbacks)
+    @feedback.activity = @activity
+    redirect_to expedition_path(@expedition)
   end
 
-  def show
+  private
+
+  def find_activity
+    @activity = Activity.find(params[:activity_id])
+  end
+
+  def set_feedbacks
+    params.require(:feedbacks).permit(:content, :rating)
   end
 end
